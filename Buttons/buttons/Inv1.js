c16a573187f499dc6aeeb1d3378a15e4d964e7c6
tarkk <flagteamzeeza@gmail.com>
2021-10-29 19:44:17 +0700

    เพิ่มปุ่ม Inv1 ให้ใช้งานได้

---
+++ Buttons/buttons/Inv1.js
@@ -0,0 +1,17 @@
+
+// const ProfileClass = require("../../add-on/ProfileClass");
+const InventoryClass = require("../../add-on/InventoryClass");
+const userconfig = require("../../Event/Config/UserSettingConfig");
+const setting = new userconfig();
+
+module.exports = {
+name: "Inv1",
+run: (i) => {
+    var user = i.user.id;
+    var userSetting = setting.get(user);
+    if (userSetting.pageID === i.message.id) {
+    //    new ProfileClass(i.message,user);
+    new InventoryClass().Weapon(i.message,user);
+    }
+}
+}
