775c7f2b1ece4e9479546a0f31dfb0ffa77ba1c7
tarkk <flagteamzeeza@gmail.com>
2021-10-29 16:15:09 +0700

    เพิ่ม Inv3

--- GUI/Buttons/InventoryButtons.js
+++ GUI/Buttons/InventoryButtons.js
@@ -3,6 +3,7 @@
 const { MessageActionRow} = require("discord.js");
 const Inv1 = require("./sub_buttons/Inv1");
 const Inv2 = require("./sub_buttons/Inv2");
+const Inv3 = require("./sub_buttons/Inv3");
 
 
 
@@ -12,7 +13,8 @@ module.exports = {
 	 const row = new MessageActionRow()
 		.addComponents(
 			Inv1.get(),
-			Inv2.get()
+			Inv2.get(),
+			Inv3.get()
 		);
 		return row;
 	}
