a016eb17817249ec76d5ff5175fff19006754a15
tarkk <flagteamzeeza@gmail.com>
2021-10-29 19:31:31 +0700

    เพิ่ม function weapon

--- add-on/InventoryClass.js
+++ add-on/InventoryClass.js
@@ -5,7 +5,7 @@ const setting = new UserSettingConfig();
 const Inventory_embed = require("../GUI/Items/Inventory_embed");
 
 module.exports = class InventoryClass{
-    constructor(message,user){
+    constructor(message = null,user = null){
         this.Create(message,user);
     }
 
@@ -30,4 +30,26 @@ module.exports = class InventoryClass{
 
 
   }
+        async Weapon(message,user){
+              
+          var userSetting = setting.get(user);
+          var embed = Inventory_embed.Weapon(user);
+          // var row = InventoryButtons.get();
+          message.channel.messages.fetch({around: userSetting.pageID, limit: 1})
+          .then(messages => {
+
+            messages.first().edit({content: `<@${user}>`,files: [], components: [],attachments: [], embeds: [embed]}).then(msg =>{
+              msg.reactions.removeAll();
+            
+        
+            });
+
+          });
+      
+
+
+
+
+      }
+
 }
