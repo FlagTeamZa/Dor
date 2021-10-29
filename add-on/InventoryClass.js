a959917e177168ce35b7cc2417c3f0e2b112792b
tarkk <flagteamzeeza@gmail.com>
2021-10-29 19:45:06 +0700

    แก้ไขบัค

--- add-on/InventoryClass.js
+++ add-on/InventoryClass.js
@@ -6,7 +6,9 @@ const Inventory_embed = require("../GUI/Items/Inventory_embed");
 
 module.exports = class InventoryClass{
     constructor(message = null,user = null){
+      if (message !== null && user !== null) {
         this.Create(message,user);
+      }
     }
 
     async Create(message,user){
@@ -31,7 +33,7 @@ module.exports = class InventoryClass{
 
   }
         async Weapon(message,user){
-              
+              // console.log(user);
           var userSetting = setting.get(user);
           var embed = Inventory_embed.Weapon(user);
           // var row = InventoryButtons.get();
