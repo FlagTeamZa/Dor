6113950f20550616cd168362cf306e5a07f6ca69
tarkk <flagteamzeeza@gmail.com>
2021-10-29 16:14:30 +0700

    กลับไปหน้าโปรไฟล์

---
+++ Buttons/buttons/Inv3.js
@@ -0,0 +1,15 @@
+
+const ProfileClass = require("../../add-on/ProfileClass");
+const userconfig = require("../../Event/Config/UserSettingConfig");
+const setting = new userconfig();
+
+module.exports = {
+name: "Inv3",
+run: (i) => {
+    var user = i.user.id;
+    var userSetting = setting.get(user);
+    if (userSetting.pageID === i.message.id) {
+       new ProfileClass(i.message,user);
+    }
+}
+}
