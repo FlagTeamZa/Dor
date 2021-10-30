933bd151d60c9ddc1d388cb0bac47998b73a6aae
tarkk <flagteamzeeza@gmail.com>
2021-10-30 18:28:13 +0700

    เพิ่ม dropdown

--- Buttons/buttons/Party2.js
+++ Buttons/buttons/Party2.js
@@ -1,17 +1,26 @@
 
 const InventoryClass = require("../../add-on/InventoryClass");
+const CreatePartyConfig = require("../../Event/Config/CreatePartyConfig");
 const userconfig = require("../../Event/Config/UserSettingConfig");
 const MapDropdown = require("../../GUI/Dropdowns/MapDropdown");
 const setting = new userconfig();
-
+const Party = new CreatePartyConfig();
 module.exports = {
 name: "Party2",
 run: async (i) => {
     var user = i.user.id;
-    var userSetting = setting.get(user);
-    if (userSetting.pageID === i.message.id) {
-        
-        i.message.send({content: `<@${user}>`,files: [], components: [],attachments: [], embeds: []});
+    // var userSetting = setting.get(user);
+    var p = Party.getParty(user);
+    var row = MapDropdown.get();
+    if (p.msgID === i.message.id) {
+ 
+        i.message.channel.send({content: `<@${user}>`,files: [], components: [row],attachments: [], embeds: []}).then(msg => {
+        Party.set(
+            user,
+            "msgID",
+            msg.id
+        );
+        });
         i.message.delete();
     }
 }
--- Buttons/buttons/Profile3.js
+++ Buttons/buttons/Profile3.js
@@ -1,14 +1,21 @@
 
 const PartyClass = require("../../add-on/PartyClass");
+const CreatePartyConfig = require("../../Event/Config/CreatePartyConfig");
 const userconfig = require("../../Event/Config/UserSettingConfig");
 const setting = new userconfig();
 // const InvClass = require("../../add-on/InventoryClass");
+const Party = new CreatePartyConfig();
 module.exports = {
 name: "Profile3",
 run: async (i) => {
     var user = i.user.id;
     var userSetting = setting.get(user);
     if (userSetting.pageID === i.message.id) {
+        Party.set(
+            user,
+            "msgID",
+            i.message.id
+        );
         new PartyClass(
             i.message,
             user
