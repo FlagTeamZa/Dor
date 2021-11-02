8700d25db05841a454b43f2224be6981e464b5ab
tarkk <flagteamzeeza@gmail.com>
2021-11-02 16:44:28 +0700

    เพิ่มสกิล

--- battle/Class_Skill/swordman.js
+++ battle/Class_Skill/swordman.js
@@ -1,8 +1,48 @@
 
-
+const mt_rand = require("../../add-on/mt_rand");
 
 module.exports = {
     class: "swordman",
+    run: (r,p) =>{
+        switch (r) {
+            case 0:
+                var ob = {
+                    "type": "buff",
+                    "name": "all-def-3-35",
+                    "msg": "%user% : บัฟเกราะ 35 % 3 เทิร์นให้เพื่อนร่วมทีมทุกคน"
+                }
+            return ob;
+
+            case 1:
+                var cri = mt_rand(1,10) < 2 ? p.Dmg * 2 : 0;
+                var dmg = (p.Dmg * 1.5) + cri;
+
+                var ob = {
+                    "type": "atk",
+                    "dmg": dmg,
+                    "msg": `%user% : สร้างความเสียหาย ${dmg} !`
+                }      
+            return ob;
+
+            case 2:
+                var dmg = p.HP - p.MaxHP;
+                var ob = {
+                    "type": "atk",
+                    "dmg": dmg,
+                    "msg": `%user% : สร้างความเสียหาย ${dmg} !`
+                }      
+            return ob;
+
+            case 3:
+                var dmg = p.Dmg;
+                var ob = {
+                    "type": "atk",
+                    "dmg": dmg,
+                    "msg": `%user% : สร้างความเสียหาย ${dmg} !`
+                }   
+            return ob;
+        }
+    },
     Skill: [
         {
             description: "ทำให้เพื่อนร่วมทีมได้รับดาเมจจากการโจมตีน้อยลง 35 % เป็นเวลา 3 เทิร์น",
